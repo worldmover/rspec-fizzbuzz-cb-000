3fed1b0d6363fefa7bb9c531c270a94ea3dc19b5
Christian Weiss <worldmover777@gmail.com>
2018-02-16 18:03:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
-def fizzbuzz
-
+def fizzbuzz(int)
+if int%3==0 && int%5==0
+  puts "FizzBuzz"
+end
+elsif int%3==0
+  puts "Fizz"
+end
+elsif int%5==0
+  puts "Buzz"
+end
+else 
+  puts nil
+end
 end
