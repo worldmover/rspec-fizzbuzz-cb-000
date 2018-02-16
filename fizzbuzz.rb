b6620bcb99c88310b0229869f11990c501973fdd
Christian Weiss <worldmover777@gmail.com>
2018-02-16 18:07:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(int)
 
-if (int%3==0 && int%5==0)
+if (int % 3 == 0 && int % 5 == 0)
   puts "FizzBuzz"
 end
 
-if (int%3==0)
+if (int % 3 == 0)
   puts "Fizz"
 end
 
-if (int%5==0)
+if (int % 5 == 0)
   puts "Buzz"
 end
 
