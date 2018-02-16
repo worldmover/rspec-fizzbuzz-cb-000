52954d250f296d66cb0466322a1e1fff168bf9ab
Christian Weiss <worldmover777@gmail.com>
2018-02-16 18:12:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@ if (int % 3 == 0 && int % 5 == 0)
   return "FizzBuzz"
 end
 
-elsif (int % 3 == 0)
+if (int % 3 == 0)
   return "Fizz"
 end
 
-elsif (int % 5 == 0)
+if (int % 5 == 0)
   return "Buzz"
 end
 
